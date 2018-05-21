0d6a8bc29ea89244f36c5128a25431151189bf78
Stephen Bau <bauhouse@gmail.com>
2018-05-21 15:10:20 -0700

    Wherefore Art Thou: methods of the Object constructor

--- frontend/algorithm_intermediate/wherefore-art-thou.js
+++ frontend/algorithm_intermediate/wherefore-art-thou.js
@@ -4,9 +4,15 @@ function whatIsInAName(collection, source) {
   var arr = [];
   // Only change code below this line
 
+  var collectionKeys = Object.keys(collection);
+  var sourceKeys = Object.keys(source);
+
+  var collectionEntries = Object.entries(collection);
+  var sourceEntries = Object.entries(source);
 
   // Only change code above this line
-  return arr;
+  // return arr;
+  return collectionEntries;
 }
 
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
