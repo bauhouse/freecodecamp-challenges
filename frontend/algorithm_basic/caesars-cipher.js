a427c8ff2e31b98b22e75feefe1225e810038774
Stephen Bau <bauhouse@gmail.com>
2018-05-21 12:13:01 -0700

    Add Caesars Cipher challenge to Basic Algorithm Scripting

---
+++ frontend/algorithm_basic/caesars-cipher.js
@@ -0,0 +1,37 @@
+var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var total = alphabet.length;
+
+function rot13(str) { // LBH QVQ VG!
+
+  var shift = 13;
+  var decodedCipher = [];
+  var letter = '';
+  var newLetter = '';
+
+  for (i = 0; i < str.length; i++) {
+    var character = str[i];
+    // Test if character is a letter of the alphabet
+    if (alphabet.indexOf(character) === -1) {
+      decodedCipher.push(character);
+    } else {
+      letter = str[i];
+      newLetter = getNewLetter(letter, shift);
+      decodedCipher.push(newLetter);
+    }
+  }
+
+  return decodedCipher.join('');
+}
+
+function getNewLetter(letter, shift) {
+  var initialIndex = alphabet.indexOf(letter);
+  var newIndex = initialIndex + shift;
+  if (newIndex > (total - 1)) {
+    newIndex = newIndex - total;
+  }
+  var newLetter = alphabet[newIndex];
+  return newLetter;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
