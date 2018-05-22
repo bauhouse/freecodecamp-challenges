c24730956bb370240ae951714269e0648f55351a
Stephen Bau <bauhouse@gmail.com>
2018-05-22 10:35:20 -0700

    Sum all fibonacci numbers less than num

--- frontend/algorithm_intermediate/sum-all-odd-fibonacci-numbers.js
+++ frontend/algorithm_intermediate/sum-all-odd-fibonacci-numbers.js
@@ -1,6 +1,16 @@
 
 function sumFibs(num) {
-  return num;
+  var fib = 1;
+  var sequence = [fib];
+
+  for ( var i = 0; i < num; i++ ) {
+    if (i) { fib = fib + sequence[i - 1] };
+    if ( fib < num ) { sequence.push(fib) };
+  }
+
+  var sum = sequence.reduce( function(acc,val) { return acc + val });
+
+  return sum;
 }
 
 sumFibs(4);
