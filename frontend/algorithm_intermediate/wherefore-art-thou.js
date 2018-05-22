4aa16ee6702c71e9ab7226221099de9c623d4c61
Stephen Bau <bauhouse@gmail.com>
2018-05-21 17:39:22 -0700

    Figuring out how to retrieve the key and value pairs in Wherefore Art Thou challenge

--- frontend/algorithm_intermediate/wherefore-art-thou.js
+++ frontend/algorithm_intermediate/wherefore-art-thou.js
@@ -13,15 +13,34 @@ function whatIsInAName(collection, source) {
   var collectionValues = Object.values(collection);
   var sourceValues = Object.values(source);
 
+  var map = collection.map(obj => Object.keys(obj));
+
+  for (var i = 0; i < collection.length; i++) {
+    var match = [];
+    for (var key in source) {
+      match.push(Object.keys(collection[i]).includes(key));
+      match.push(Object.values(collection[i]).includes(source[key]));
+    }
+    if (!match.includes(false)) {
+      arr.push(collection[i]);
+    }
+  }
+
   return JSON.stringify(Object.values(collection))
     + '\n' + 'length of collection[2]: ' + Object.keys(collection[2]).length
     + '\n' + 'length of source: ' + Object.keys(source).length
     + '\n' + 'keys in collection[2]: ' + Object.keys(collection[2])
-    + '\n' + 'key: ' + Object.keys(source)
-    + '\n' + 'value: ' + Object.values(source);
+    + '\n' + 'keys in source: ' + Object.keys(source)
+    + '\n' + 'value: ' + Object.values(source)
+    + '\n'
+    + '\n' + JSON.stringify(source)
+    + '\n'
+    + '\n' + JSON.stringify(map)
+    + '\n'
+    + '\n' + JSON.stringify(arr);
 
   // Only change code above this line
   // return arr;
 }
 
-whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 });
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
