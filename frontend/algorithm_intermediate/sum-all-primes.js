fb114896bf9271543a1122ec2491e181443c4801
Stephen Bau <bauhouse@gmail.com>
2018-05-22 11:44:52 -0700

    The test for prime numbers
    Can be divided by a smaller integer other than 1

--- frontend/algorithm_intermediate/sum-all-primes.js
+++ frontend/algorithm_intermediate/sum-all-primes.js
@@ -1,6 +1,18 @@
 
 function sumPrimes(num) {
-  return num;
+  var integers = [];
+  var primes = [];
+
+  for (var i = 1; i < num; i++) {
+    var divisors = integers.filter( value=> value < i );
+    primes.push([i, divisors.filter( value => i % value == 0 && value != 1 )]);
+    integers.push(i);
+  }
+
+  // primes = integers.filter( value => );
+
+
+  return primes;
 }
 
-sumPrimes(10);
+console.log(sumPrimes(10));
