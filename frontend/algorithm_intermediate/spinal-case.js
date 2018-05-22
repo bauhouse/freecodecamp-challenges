c3a123f7f7a15845c71062008e6abef1b4cad216
Stephen Bau <bauhouse@gmail.com>
2018-05-22 09:55:25 -0700

    Refactor Spinal Case challenge as a single return statement with comments

--- frontend/algorithm_intermediate/spinal-case.js
+++ frontend/algorithm_intermediate/spinal-case.js
@@ -2,29 +2,21 @@
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
-
-  // Convert camel case to words by adding spaces in front of capital letters
-  str = str.split('').map( char => char == char.toUpperCase() ? ' ' + char : char ).join('');
-
-  // Convert to lower case
-  str = str.toLowerCase();
-
-  // Replace underscores with spaces
-  str = str.replace(/_/g, ' ');
-
-  // Replace multiple spaces with single space
-  str = str.replace(/  +/g, ' ');
-
-  // Remove spaces at the beginning of the string
-  str = str.replace(/^\s+/, '');
-
-  // Replace spaces with hyphens
-  str = str.replace(/\s/g, '-');
-
-  // Replace multiple hyphens with single hyphen
-  str = str.replace(/--+/g, '-');
-
-  return str;
+  return str.split('')
+    // Convert camel case to words by adding spaces in front of capital letters
+    .map( char => char == char.toUpperCase() ? ' ' + char : char ).join('')
+    // Convert to lower case
+    .toLowerCase()
+    // Replace underscores with spaces
+    .replace(/_/g, ' ')
+    // Replace multiple spaces with single space
+    .replace(/  +/g, ' ')
+    // Remove spaces at the beginning of the string
+    .replace(/^\s+/, '')
+    // Replace spaces with hyphens
+    .replace(/\s/g, '-')
+    // Replace multiple hyphens with single hyphen
+    .replace(/--+/g, '-');
 }
 
 spinalCase('Teletubbies say Eh-oh');
