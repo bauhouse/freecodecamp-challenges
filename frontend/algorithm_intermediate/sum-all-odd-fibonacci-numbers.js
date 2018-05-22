a9dec9baa6c6edffb9e6b8d666496ba88afe5e47
Stephen Bau <bauhouse@gmail.com>
2018-05-22 10:51:40 -0700

    Sum All Odd Fibonacci Numbers challenge completed

--- frontend/algorithm_intermediate/sum-all-odd-fibonacci-numbers.js
+++ frontend/algorithm_intermediate/sum-all-odd-fibonacci-numbers.js
@@ -5,7 +5,7 @@ function sumFibs(num) {
 
   for ( var i = 0; i < num; i++ ) {
     if (i) { fib = fib + sequence[i - 1] };
-    if ( fib < num ) { sequence.push(fib) };
+    if ( fib <= num ) { sequence.push(fib) };
   }
 
   var sum = sequence.reduce( function(acc,val) {return acc + val;});
