894b1ec771708da9ef94346e11eae9ab89f6d363
Stephen Bau <bauhouse@gmail.com>
2018-05-24 11:43:02 -0700

    Still having issues with arrays in the arguments

--- frontend/algorithm_intermediate/arguments-optional.js
+++ frontend/algorithm_intermediate/arguments-optional.js
@@ -1,6 +1,25 @@
 
 function addTogether() {
-  return false;
+  const args = Array.from(arguments);
+  var x = args[0];
+  if (args.length > 1) {
+    var includesArr = args.map(arg => {
+      return Array.isArray(arg);
+    }).includes(true);
+    if (includesArr) {
+      return undefined;
+    }
+    var y = args[1];
+    if (typeof x !== "number" || typeof y !== "number") {
+      return undefined;
+    } else {
+      return x + y;
+    }
+  } else {
+    return function(y) {
+      return x + y;
+    };
+  }
 }
 
 addTogether(2,3);
