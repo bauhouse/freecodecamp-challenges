c803872a1eadf60a3f664cfc9ca194e6723c703b
Stephen Bau <bauhouse@gmail.com>
2018-05-23 23:48:18 -0700

    Drop It challenge completed

--- frontend/algorithm_intermediate/drop-it.js
+++ frontend/algorithm_intermediate/drop-it.js
@@ -1,6 +1,15 @@
 
 function dropElements(arr, func) {
   // Drop them elements.
+  var count = arr.length;
+
+  for (var i = 0; i < count; i++) {
+    if (func(arr[0])) {
+      break;
+    } else {
+      arr.shift();
+    }
+  }
   return arr;
 }
 
