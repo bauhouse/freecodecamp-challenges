12da7861ba0c78d1127cb18eeb3e6118b2595747
Stephen Bau <bauhouse@gmail.com>
2018-05-24 10:39:43 -0700

    Refactor Everything Be True to remove unnecessary variables

--- frontend/algorithm_intermediate/everything-be-true.js
+++ frontend/algorithm_intermediate/everything-be-true.js
@@ -1,27 +1,21 @@
 
 function truthCheck(collection, pre) {
-  var fail = [];
   var check = [];
-  var arr = collection.map(obj => {
+  collection.map(obj => {
     var propExists = obj.hasOwnProperty(pre);
     if (!propExists) {
-      fail.push("exists: false");
       check.push(propExists);
     }
     var x = obj[pre];
     if (x === 0 || x === null || x === undefined) {
-      fail.push("0, null, undefined");
       check.push(false);
     }
     if (typeof x === "string" && x.length === 0) {
-      fail.push("empty string");
       check.push(false);
     }
     if (typeof x === "number" && isNaN(x)) {
-      fail.push("NaN");
       check.push(false);
     }
-    return fail;
   });
   check = !check.includes(false);
   // Is everyone being true?
