be63737880c40c7474a887c9ac5bdf9a271d04ed
Stephen Bau <bauhouse@gmail.com>
2018-05-24 10:36:56 -0700

    Everything Be True challenge completed

--- frontend/algorithm_intermediate/everything-be-true.js
+++ frontend/algorithm_intermediate/everything-be-true.js
@@ -1,7 +1,31 @@
 
 function truthCheck(collection, pre) {
+  var fail = [];
+  var check = [];
+  var arr = collection.map(obj => {
+    var propExists = obj.hasOwnProperty(pre);
+    if (!propExists) {
+      fail.push("exists: false");
+      check.push(propExists);
+    }
+    var x = obj[pre];
+    if (x === 0 || x === null || x === undefined) {
+      fail.push("0, null, undefined");
+      check.push(false);
+    }
+    if (typeof x === "string" && x.length === 0) {
+      fail.push("empty string");
+      check.push(false);
+    }
+    if (typeof x === "number" && isNaN(x)) {
+      fail.push("NaN");
+      check.push(false);
+    }
+    return fail;
+  });
+  check = !check.includes(false);
   // Is everyone being true?
-  return pre;
+  return check;
 }
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
