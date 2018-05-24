b295c5ac131b65b284ba2295ff450b2a1ac92f77
Stephen Bau <bauhouse@gmail.com>
2018-05-24 00:37:30 -0700

    Recursive function to find nested arrays

--- frontend/algorithm_intermediate/steamroller.js
+++ frontend/algorithm_intermediate/steamroller.js
@@ -1,7 +1,27 @@
 
 function steamrollArray(arr) {
   // I'm a steamroller, baby
-  return arr;
+  var nested = [];
+  var level = 0;
+  if (Array.isArray(arr)) {
+    nested.push([level, arr]);
+    findArrays(arr, level, nested);
+  }
+  return nested;
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+function findArrays(arr, level, nested, isArr = false) {
+  var isArr = Array.isArray(arr);
+  if (isArr) {
+    level++;
+    for (var i = 0; i < arr.length; i++) {
+      nested.push([level, arr[i], isArr]);
+      findArrays(arr[i], level, nested, isArr);
+      // console.log('this is an array: ' + arr[i]);
+      // console.log('level: ' + level);
+    }
+  }
+  return 'Array recursion completed';
+}
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
