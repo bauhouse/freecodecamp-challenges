545c5f6f1e5c294bc5ef8b43c1359bde789281e8
Stephen Bau <bauhouse@gmail.com>
2018-05-24 15:02:29 -0700

    Replace check array with boolean variable for Everything Be True challenge

--- frontend/algorithm_intermediate/everything-be-true.js
+++ frontend/algorithm_intermediate/everything-be-true.js
@@ -1,25 +1,24 @@
 
 function truthCheck(collection, pre) {
-  var check = [];
+  var check = true;
   collection.map(obj => {
     var propExists = obj.hasOwnProperty(pre);
     if (!propExists) {
-      check.push(propExists);
+      check = false;
     }
     var x = obj[pre];
     if (x === 0 || x === null || x === undefined) {
-      check.push(false);
+      check = false;
     }
     if (typeof x === "string" && x.length === 0) {
-      check.push(false);
+      check = false;
     }
     if (typeof x === "number" && isNaN(x)) {
-      check.push(false);
+      check = false;
     }
   });
-  check = !check.includes(false);
   // Is everyone being true?
   return check;
 }
 
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
