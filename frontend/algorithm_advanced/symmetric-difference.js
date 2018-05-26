9d6ec3b0dd337c3f2258ee8ac9ad63b68f41b6e4
Stephen Bau <bauhouse@gmail.com>
2018-05-26 13:52:19 -0700

    Create a compareArrays() function

--- frontend/algorithm_advanced/symmetric-difference.js
+++ frontend/algorithm_advanced/symmetric-difference.js
@@ -3,6 +3,11 @@ function sym(args) {
   var arr = Array.prototype.slice.call(arguments);
   var arr1 = arr[0];
   var arr2 = arr[1];
+  var diff = compareArrays(arr1, arr2);
+  return diff;
+}
+
+function compareArrays(arr1, arr2, type = "diff") {
   var diff = arr1.concat(arr2);
   var intersection = arr1.filter(value => {
     return arr2.includes(value);
@@ -12,8 +17,11 @@ function sym(args) {
       return value !== item;
     });
   });
-
-  return diff;
+  if (type === "diff") {
+    return diff;
+  } else {
+    return intersection;
+  }
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
