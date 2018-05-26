6196a99aad342d1b43f55b3417d64bb6501867e4
Stephen Bau <bauhouse@gmail.com>
2018-05-26 13:34:54 -0700

    Find the symmetric difference between two arrays of numbers

--- frontend/algorithm_advanced/symmetric-difference.js
+++ frontend/algorithm_advanced/symmetric-difference.js
@@ -1,6 +1,19 @@
 
 function sym(args) {
-  return args;
+  var arr = Array.prototype.slice.call(arguments);
+  var arr1 = arr[0];
+  var arr2 = arr[1];
+  var diff = arr1.concat(arr2);
+  var intersection = arr1.filter(value => {
+    return arr2.includes(value);
+  });
+  intersection.map(item => {
+    diff = diff.filter(value => {
+      return value !== item;
+    });
+  });
+
+  return diff;
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
