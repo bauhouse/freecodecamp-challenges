3dcc655a6c38051239aa4cea43c895c2b3005c33
Stephen Bau <bauhouse@gmail.com>
2018-05-27 14:26:53 -0700

    Inventory the currency denominations, value, amount and number of each

--- frontend/algorithm_advanced/exact-change.js
+++ frontend/algorithm_advanced/exact-change.js
@@ -3,19 +3,40 @@ function checkCashRegister(price, cash, cid) {
   var change;
   var diff = cash - price;
 
-  var currency = [
-    ["ONE HUNDRED", 100],
-    ["TWENTY", 20],
-    ["FIVE", 5],
-    ["ONE", 1],
-    ["QUARTER", 0.25],
-    ["DIME", 0.10],
-    ["NICKEL", 0.05],
-    ["PENNY", 0.01]
-  ];
+  var currency = {
+    "ONE HUNDRED": 100,
+    "TWENTY": 20,
+    "FIVE": 5,
+    "ONE": 1,
+    "QUARTER": 0.25,
+    "DIME": 0.10,
+    "NICKEL": 0.05,
+    "PENNY": 0.01
+  };
+
+  var denominations = [];
+
+  for (var key in currency) {
+    var denomination = key;
+    var value = currency[key];
+    denominations.push([denomination, value]);
+  }
+
+  var inventory = denominations;
+
+  inventory = inventory.map((item, index, array) => {
+    var denom = array[index][0];
+    var val = array[index][1];
+    var amount = cid.filter(arr => {
+      return arr[0] == denom;
+    })[0][1];
+    var num = Math.round(amount / val);
+    // amount = amount[1];
+    return [denom, val, amount, num];
+  });
 
   // Here is your change, ma'am.
-  return change;
+  return inventory;
 }
 
 // Example cash-in-drawer array:
