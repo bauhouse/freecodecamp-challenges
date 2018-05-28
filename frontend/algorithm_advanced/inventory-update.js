e9b97bbf00422689478f905596906f1339129621
Stephen Bau <bauhouse@gmail.com>
2018-05-27 19:48:07 -0700

    Update current inventory array

--- frontend/algorithm_advanced/inventory-update.js
+++ frontend/algorithm_advanced/inventory-update.js
@@ -1,22 +1,38 @@
 
 function updateInventory(arr1, arr2) {
-    // All inventory must be accounted for or you're fired!
-    return arr1;
+  // All inventory must be accounted for or you're fired!
+  for (var i = 0; i < arr2.length; i++) {
+    var match = false;
+    var item = arr2[i];
+    var quantity = item[0];
+    var name = item[1];
+    for (var j = 0; j < arr1.length; j++) {
+      if (arr1[j][1] === name) {
+        arr1[j][0] += quantity;
+        match = true;
+      }
+    }
+    if (match === false) {
+      arr1.push(item);
+    }
+  }
+
+  return arr1;
 }
 
 // Example inventory lists
 var curInv = [
-    [21, "Bowling Ball"],
-    [2, "Dirty Sock"],
-    [1, "Hair Pin"],
-    [5, "Microphone"]
+  [21, "Bowling Ball"],
+  [2, "Dirty Sock"],
+  [1, "Hair Pin"],
+  [5, "Microphone"]
 ];
 
 var newInv = [
-    [2, "Hair Pin"],
-    [3, "Half-Eaten Apple"],
-    [67, "Bowling Ball"],
-    [7, "Toothpaste"]
+  [2, "Hair Pin"],
+  [3, "Half-Eaten Apple"],
+  [67, "Bowling Ball"],
+  [7, "Toothpaste"]
 ];
 
 updateInventory(curInv, newInv);
