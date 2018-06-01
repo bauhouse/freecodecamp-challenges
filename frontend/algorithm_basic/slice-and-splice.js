99ee22b9db94d4b05b7a2a57a6864e685ca1231a
Stephen Bau <bauhouse@gmail.com>
2018-05-31 21:12:38 -0700

    Slice and Splice challenge completed

--- frontend/algorithm_basic/slice-and-splice.js
+++ frontend/algorithm_basic/slice-and-splice.js
@@ -1,6 +1,9 @@
 function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
-  return arr2;
+  var array1 = arr1.slice();
+  var array2 = arr2.slice();
+  var array3 = array2.splice(n);
+  return array2.concat(array1).concat(array3);
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
