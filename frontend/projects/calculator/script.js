24fff5f2aa8c3533106bfa05ec97e4c110013704
Stephen Bau <bauhouse@gmail.com>
2018-06-20 22:08:35 -0700

    Modify calculator to use global variables instead of passing around an object
    Use prototype function for string replacement to output expression to
    console

--- frontend/projects/calculator/script.js
+++ frontend/projects/calculator/script.js
@@ -7,174 +7,166 @@
 var display = document.getElementById("display");
 var buttons = document.getElementsByClassName("button");
 
-var calculator = {
-  mode: "",
-  type: "",
-  input: [],
-  tape: [],
-  expression: "",
-  str: "",
-  operand: null,
-  x: null,
-  y: null,
-  operator: "",
-  float: false,
-  result: 0
-}
+var mode = "";
+var type = "";
+var input = [];
+var tape = [];
+var expression = "";
+var str = "";
+var operand = null;
+var x = null;
+var y = null;
+var operator = "";
+var float = false;
+var result = 0;
 
-init(calculator);
+init();
 
-function init(c) {
+function init() {
   for (var i = 0; i < buttons.length; i++) {
     var button = buttons[i];
     button.addEventListener("click", function( event ) {
       var value = this.value;
 
       // Get input type
-      c = inputType(this, c);
+      inputType(this);
 
       // Get input
-      switch (c.type) {
+      switch (type) {
         case "clear":
-          c = clear(c);
+          clear();
           break;
         case "calculate":
-          c = enterEquals(value, c);
+          enterEquals(value);
           break;
         case "decimal":
-          c = enterDecimal(value, c);
+          enterDecimal(value);
           break;
         case "digit":
-          c = enterDigit(value, c);
+          enterDigit(value);
           break;
         case "operator":
-          c = enterOperator(value, c);
+          enterOperator(value);
       }
 
     });
   }
 }
 
-function inputType(button, c) {
+function inputType(button) {
   var id = button.id;
   var classes = button.classList;
 
   // Get input type
   if (id === "clear") {
-    c.type = "clear";
+    type = "clear";
   } else if (id === "equals") {
-    c.type = "calculate";
+    type = "calculate";
   } else if (id === "decimal") {
-    c.type = "decimal";
+    type = "decimal";
   } else if (classes.contains("digit")) {
-    c.type = "digit";
+    type = "digit";
   } else if (classes.contains("operator")) {
-    c.type = "operator";
+    type = "operator";
   }
+}
 
+function clear() {
+  mode = "";
+  type = "";
+  input = [];
+  tape = [];
+  expression = "";
+  str = "";
+  operand = null;
+  x = null;
+  y = null;
+  operator = "";
+  float = false;
+  result = 0;
+  displayResult(result);
   return c;
 }
 
-function clear(c) {
-  c.mode = "";
-  c.type = "";
-  c.input = [];
-  c.tape = [];
-  c.expression = "";
-  c.str = "";
-  c.operand = null;
-  c.x = null;
-  c.y = null;
-  c.operator = "";
-  c.float = false;
-  c.result = 0;
-  displayResult(c.result);
-  return c;
+function enterDigit(value) {
+  if (mode === "calculate") {
+    clear();
+  }
+  input.push(value);
+  str = input.join('');
+  displayResult(str);
+  mode = "input";
 }
 
-function enterDigit(value, c) {
-  if (c.mode === "calculate") {
-    clear(c);
+function enterDecimal(value) {
+  if (mode === "calculate") {
+    clear();
   }
-  c.input.push(value);
-  c.str = c.input.join('');
-  displayResult(c.str);
-  c.mode = "input";
-  return c;
+  if (!float) {
+    input.push(value);
+    float = true;
+    str = input.join('');
+  }
+  displayResult(str);
+  mode = "input";
 }
 
-function enterDecimal(value, c) {
-  if (c.mode === "calculate") {
-    clear(c);
-  }
-  if (!c.float) {
-    c.input.push(value);
-    c.float = true;
-    c.str = c.input.join('');
-  }
-  displayResult(c.str);
-  c.mode = "input";
-  return c;
-}
-
-function enterOperator(value, c) {
+function enterOperator(value) {
   // If the previous mode was "input",
   // enter the current number and calculate
-  if (c.mode === "input") {
-    var num = enterNumber(c);
-    if (c.x && !c.y) {
-      c.result = num;
+  if (mode === "input") {
+    var num = enterNumber();
+    if (x && !y) {
+      result = num;
     }
-    resetInput(c);
-    calculateResult(c);
-    displayResult(c.result);
-    printTape(c);
-    c.x = c.result;
+    resetInput();
+    calculateResult();
+    displayResult(result);
+    printTape();
+    x = result;
   }
 
   // Then update the operator
-  c.operator = value;
-  if (c.mode === "operator") {
-    c.tape.pop();
-    c.tape.push(value);
+  operator = value;
+  if (mode === "operator") {
+    tape.pop();
+    tape.push(value);
   } else {
-    c.tape.push(value);
+    tape.push(value);
   }
-  c.mode = "operator";
-  return c;
+  mode = "operator";
 }
 
-function enterEquals(value, c) {
-  enterNumber(c);
-  resetInput(c);
-  calculateResult(c);
-  displayResult(c.result);
-  printTape(c);
-  c.x = c.result;
-  c.mode = "calculate";
-  return c;
+function enterEquals(value) {
+  enterNumber();
+  resetInput();
+  calculateResult();
+  displayResult(result);
+  printTape();
+  x = result;
+  mode = "calculate";
 }
 
-function enterNumber(c) {
-  c.operand = Number(c.str);
-  if (c.mode === "input") {
-    c.tape.push(c.operand);
+function enterNumber() {
+  operand = Number(str);
+  if (mode === "input") {
+    tape.push(operand);
   }
-  if (c.x === null) {
-    c.x = c.operand;
+  if (x === null) {
+    x = operand;
   } else {
-    c.y = c.operand;
+    y = operand;
   }
-  return c.operand;
+  return operand;
 }
 
-function calculateResult(c) {
-  if (c.y) {
-    if (c.operator) {
-      c.result = calculate(c.x, c.y, c.operator);
+function calculateResult() {
+  if (y) {
+    if (operator) {
+      result = calculate(x, y, operator);
     }
   }
-  return c.result;
+  return result;
 }
 
 function calculate(x, y, operator) {
@@ -206,40 +198,47 @@ function displayResult(result) {
   return result;
 }
 
-function translate(str) {
-  return str.replace('*','×').replace('/', '÷');
+function resetInput() {
+  input = [];
+  float = false;
 }
 
-function resetInput(c) {
-  c.input = [];
-  c.float = false;
-  return c;
-}
-
-function resetOperator(c) {
-  c.operator = "";
-  return c;
+function resetOperator() {
+  operator = "";
 }
 
 function printTape(c) {
-  c.expression = c.tape.join(' ');
-  c.expression = translate(c.expression);
-  console.log(c.expression);
-  console.log("= " + c.result);
-  return c.expression;
+  expression = tape.join(' ');
+  expression = translate(expression);
+  console.log(expression);
+  console.log("= " + result);
+  return expression;
 }
 
+function translate(str) {
+  var regex = /\*/;
+  str = str.replaceAll(regex,'×')
+  regex = /\//;
+  str = str.replaceAll(regex,'÷')
+  return str;
+}
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
 function debugCalculator(c) {
-  console.log("mode: " + c.mode);
-  console.log("type: " + c.type);
-  console.log("input: " + c.input);
-  console.log("tape: " + c.tape);
-  console.log("expression: " + c.expression);
-  console.log("str: " + c.str);
-  console.log("operand: " + c.operand);
-  console.log("x: " + c.x);
-  console.log("y: " + c.y);
-  console.log("operator: " + c.operator);
-  console.log("float: " + c.float);
-  console.log("result: " + c.result);
+  console.log("mode: " + mode);
+  console.log("type: " + type);
+  console.log("input: " + input);
+  console.log("tape: " + tape);
+  console.log("expression: " + expression);
+  console.log("str: " + str);
+  console.log("operand: " + operand);
+  console.log("x: " + x);
+  console.log("y: " + y);
+  console.log("operator: " + operator);
+  console.log("float: " + float);
+  console.log("result: " + result);
 }
