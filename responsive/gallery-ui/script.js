6f8730cd84fdfe4b67afd526d15ce421ac92eeac
Stephen Bau <bauhouse@gmail.com>
2018-06-25 15:30:39 -0700

    Create Unsplash photo gallery with direction-aware hover effect

--- responsive/gallery-ui/script.js
+++ responsive/gallery-ui/script.js
@@ -1,4 +1,5 @@
 var main = function() {
+
   $('.menu-button').click(function() {
     $('.menu').animate({left:'0px'}, 200);
     $('body').animate({left:'320px'}, 250).addClass('open');
@@ -8,6 +9,9 @@ var main = function() {
     $('.menu').animate({left:'-320px'}, 200);
     $('body').animate({left:'0px'}, 200).removeClass('open');
   });
+
+  $(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
+
 };
 
 $(document).ready(main);
