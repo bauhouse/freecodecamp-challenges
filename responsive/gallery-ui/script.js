1c0110d8db907145ff104fb5c0d04d634364a311
Stephen Bau <bauhouse@gmail.com>
2018-06-25 16:51:38 -0700

    Push menu squeezes content by increasing the left margin

--- responsive/gallery-ui/script.js
+++ responsive/gallery-ui/script.js
@@ -1,13 +1,13 @@
 var main = function() {
 
   $('.menu-button').click(function() {
-    $('.menu').animate({left:'0px'}, 200);
-    $('body').animate({left:'320px'}, 250).addClass('open');
+    $('.menu').animate({left: 0}, 200);
+    $('body').animate({marginLeft: '320px'}, 250).addClass('open');
     $('.menu-button').click(false);
   });
   $('.close-button').click(function() {
-    $('.menu').animate({left:'-320px'}, 200);
-    $('body').animate({left:'0px'}, 200).removeClass('open');
+    $('.menu').animate({left: '-320px'}, 200);
+    $('body').animate({marginLeft: 0}, 200).removeClass('open');
   });
 
   $(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
