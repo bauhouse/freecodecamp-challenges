0d7c3ca4e5204e41fc42c810c1d6e7ca05b35f03
Stephen Bau <bauhouse@gmail.com>
2018-06-24 21:40:33 -0700

    Display random quote

--- frontend/projects/random-quote-machine/script.js
+++ frontend/projects/random-quote-machine/script.js
@@ -7,8 +7,16 @@
 var display = document.getElementById("display");
 var buttons = document.getElementsByClassName("button");
 var dataURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
-var quotes = null;
+var quotesJSON = null;
+var quotes = [];
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+var quote = {
+  num: 1,
+  text: null,
+  author: null
+};
+var displayText = document.getElementById("text");
+var displayAuthor = document.getElementById("author");
 
 var key = {id: null, name: null, code: null};
 
@@ -25,10 +33,38 @@ function init() {
   keyboard();
 }
 
+function getQuotes(data) {
+  quotesJSON = parseJSON(data);
+  quotes = quotesJSON.quotes;
+  getRandomQuote(quotes);
+}
+
+function getRandomQuote() {
+  var num = randomNumber(quote.num, quotes.length);
+  getCurrentQuote(num);
+}
+
+function getCurrentQuote(num) {
+  quote.num = num;
+  quote.text = quotes[num].quote;
+  quote.author = quotes[num].author;
+  displayCurrentQuote();
+}
+
+function displayCurrentQuote() {
+  displayText.innerText = quote.text;
+  displayAuthor.innerText = "— " + quote.author;
+}
+
 function getInput(button) {
   key.id = button.id;
   key.name = button.dataset.key;
   key.code = button.dataset.keycode;
+  eventCallback();
+}
+
+function eventCallback() {
+  getRandomQuote();
 }
 
 function keyboard() {
@@ -54,11 +90,6 @@ function keyboardEvents(keyEvent) {
       }
     }
   });
-}
-
-function getQuotes(data) {
-  quotes = parseJSON(data);
-  console.log(quotes);
 }
 
 function getJSON(url, func) {
@@ -95,4 +126,13 @@ function loadFile(url, timeout, callback) {
   xhr.open("GET", url, true);
   xhr.timeout = timeout;
   xhr.send(null);
+}
+
+function randomNumber(current, num) {
+  var num = Math.floor(Math.random() * num);
+  if (current == num) {
+    return randomNumber(current, num);
+  } else {
+    return num;
+  }
 }
