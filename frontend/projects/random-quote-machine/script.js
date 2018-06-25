28754a8a3f2aa7c4eb9edad9b80389f82cc45084
Stephen Bau <bauhouse@gmail.com>
2018-06-24 19:10:13 -0700

    Load JSON file for quotes

--- frontend/projects/random-quote-machine/script.js
+++ frontend/projects/random-quote-machine/script.js
@@ -6,6 +6,8 @@
 
 var display = document.getElementById("display");
 var buttons = document.getElementsByClassName("button");
+var dataURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+var quotes = null;
 
 var key = {id: null, name: null, code: null};
 var sound = null;
@@ -13,6 +15,7 @@ var sound = null;
 init();
 
 function init() {
+  getJSON(dataURL, getQuotes);
   for (var i = 0; i < buttons.length; i++) {
     var button = buttons[i];
     button.addEventListener("click", function( event ) {
@@ -51,4 +54,45 @@ function keyboardEvents(keyEvent) {
       }
     }
   });
+}
+
+function getQuotes(data) {
+  quotes = parseJSON(data);
+  console.log(quotes);
+}
+
+function getJSON(url, func) {
+  loadFile(url, 2000, loadJSON, func);
+}
+
+function loadJSON(func) {
+  var data = this.responseText;
+  func(data);
+}
+
+function parseJSON(text) {
+  if (text) {
+    return JSON.parse(text);
+  }
+  return null;
+}
+
+function loadFile(url, timeout, callback) {
+  var args = Array.prototype.slice.call(arguments, 3);
+  var xhr = new XMLHttpRequest();
+  xhr.ontimeout = function () {
+    console.error("The request for " + url + " timed out.");
+  };
+  xhr.onload = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        callback.apply(xhr, args);
+      } else {
+        console.error(xhr.statusText);
+      }
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.timeout = timeout;
+  xhr.send(null);
 }
