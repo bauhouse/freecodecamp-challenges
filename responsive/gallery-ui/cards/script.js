9d86ef6112380154572ad190bb65c0cf6e81c3cb
Stephen Bau <bauhouse@gmail.com>
2018-06-25 22:27:49 -0700

    Implement lazy loading (see sitepoint article in script.js)

--- responsive/gallery-ui/cards/script.js
+++ responsive/gallery-ui/cards/script.js
@@ -15,212 +15,212 @@
  */
 ;( function( $, window, undefined ) {
 
-	'use strict';
+  'use strict';
 
-	$.HoverDir = function( options, element ) {
+  $.HoverDir = function( options, element ) {
 
-		this.$el = $( element );
-		this._init( options );
+    this.$el = $( element );
+    this._init( options );
 
-	};
+  };
 
-	// the options
-	$.HoverDir.defaults = {
-		speed : 300,
-		easing : 'ease',
-		hoverDelay : 0,
-		inverse : false
-	};
+  // the options
+  $.HoverDir.defaults = {
+    speed : 300,
+    easing : 'ease',
+    hoverDelay : 0,
+    inverse : false
+  };
 
-	$.HoverDir.prototype = {
+  $.HoverDir.prototype = {
 
-		_init : function( options ) {
+    _init : function( options ) {
 
-			// options
-			this.options = $.extend( true, {}, $.HoverDir.defaults, options );
-			// transition properties
-			this.transitionProp = 'all ' + this.options.speed + 'ms ' + this.options.easing;
-			// support for CSS transitions
-			this.support = Modernizr.csstransitions;
-			// load the events
-			this._loadEvents();
-
-		},
-		_loadEvents : function() {
-
-			var self = this;
-
-			this.$el.on( 'mouseenter.hoverdir, mouseleave.hoverdir', function( event ) {
-
-				var $el = $( this ),
-					$hoverElem = $el.find( 'div' ),
-					direction = self._getDir( $el, { x : event.pageX, y : event.pageY } ),
-					styleCSS = self._getStyle( direction );
-
-				if( event.type === 'mouseenter' ) {
+      // options
+      this.options = $.extend( true, {}, $.HoverDir.defaults, options );
+      // transition properties
+      this.transitionProp = 'all ' + this.options.speed + 'ms ' + this.options.easing;
+      // support for CSS transitions
+      this.support = Modernizr.csstransitions;
+      // load the events
+      this._loadEvents();
+
+    },
+    _loadEvents : function() {
+
+      var self = this;
+
+      this.$el.on( 'mouseenter.hoverdir, mouseleave.hoverdir', function( event ) {
+
+        var $el = $( this ),
+          $hoverElem = $el.find( 'div' ),
+          direction = self._getDir( $el, { x : event.pageX, y : event.pageY } ),
+          styleCSS = self._getStyle( direction );
+
+        if( event.type === 'mouseenter' ) {
 
-					$hoverElem.hide().css( styleCSS.from );
-					clearTimeout( self.tmhover );
+          $hoverElem.hide().css( styleCSS.from );
+          clearTimeout( self.tmhover );
 
-					self.tmhover = setTimeout( function() {
+          self.tmhover = setTimeout( function() {
 
-						$hoverElem.show( 0, function() {
+            $hoverElem.show( 0, function() {
 
-							var $el = $( this );
-							if( self.support ) {
-								$el.css( 'transition', self.transitionProp );
-							}
-							self._applyAnimation( $el, styleCSS.to, self.options.speed );
+              var $el = $( this );
+              if( self.support ) {
+                $el.css( 'transition', self.transitionProp );
+              }
+              self._applyAnimation( $el, styleCSS.to, self.options.speed );
 
-						} );
+            } );
 
 
-					}, self.options.hoverDelay );
+          }, self.options.hoverDelay );
 
-				}
-				else {
+        }
+        else {
 
-					if( self.support ) {
-						$hoverElem.css( 'transition', self.transitionProp );
-					}
-					clearTimeout( self.tmhover );
-					self._applyAnimation( $hoverElem, styleCSS.from, self.options.speed );
+          if( self.support ) {
+            $hoverElem.css( 'transition', self.transitionProp );
+          }
+          clearTimeout( self.tmhover );
+          self._applyAnimation( $hoverElem, styleCSS.from, self.options.speed );
 
-				}
+        }
 
-			} );
+      } );
 
-		},
-		// credits : http://stackoverflow.com/a/3647634
-		_getDir : function( $el, coordinates ) {
+    },
+    // credits : http://stackoverflow.com/a/3647634
+    _getDir : function( $el, coordinates ) {
 
-			// the width and height of the current div
-			var w = $el.width(),
-				h = $el.height(),
+      // the width and height of the current div
+      var w = $el.width(),
+        h = $el.height(),
 
-				// calculate the x and y to get an angle to the center of the div from that x and y.
-				// gets the x value relative to the center of the DIV and "normalize" it
-				x = ( coordinates.x - $el.offset().left - ( w/2 )) * ( w > h ? ( h/w ) : 1 ),
-				y = ( coordinates.y - $el.offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
+        // calculate the x and y to get an angle to the center of the div from that x and y.
+        // gets the x value relative to the center of the DIV and "normalize" it
+        x = ( coordinates.x - $el.offset().left - ( w/2 )) * ( w > h ? ( h/w ) : 1 ),
+        y = ( coordinates.y - $el.offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
 
-				// the angle and the direction from where the mouse came in/went out clockwise (TRBL=0123);
-				// first calculate the angle of the point,
-				// add 180 deg to get rid of the negative values
-				// divide by 90 to get the quadrant
-				// add 3 and do a modulo by 4  to shift the quadrants to a proper clockwise TRBL (top/right/bottom/left) **/
-				direction = Math.round( ( ( ( Math.atan2(y, x) * (180 / Math.PI) ) + 180 ) / 90 ) + 3 ) % 4;
+        // the angle and the direction from where the mouse came in/went out clockwise (TRBL=0123);
+        // first calculate the angle of the point,
+        // add 180 deg to get rid of the negative values
+        // divide by 90 to get the quadrant
+        // add 3 and do a modulo by 4  to shift the quadrants to a proper clockwise TRBL (top/right/bottom/left) **/
+        direction = Math.round( ( ( ( Math.atan2(y, x) * (180 / Math.PI) ) + 180 ) / 90 ) + 3 ) % 4;
 
-			return direction;
+      return direction;
 
-		},
-		_getStyle : function( direction ) {
+    },
+    _getStyle : function( direction ) {
 
-			var fromStyle, toStyle,
-				slideFromTop = { left : '0px', top : '-100%' },
-				slideFromBottom = { left : '0px', top : '100%' },
-				slideFromLeft = { left : '-100%', top : '0px' },
-				slideFromRight = { left : '100%', top : '0px' },
-				slideTop = { top : '0px' },
-				slideLeft = { left : '0px' };
+      var fromStyle, toStyle,
+        slideFromTop = { left : '0px', top : '-100%' },
+        slideFromBottom = { left : '0px', top : '100%' },
+        slideFromLeft = { left : '-100%', top : '0px' },
+        slideFromRight = { left : '100%', top : '0px' },
+        slideTop = { top : '0px' },
+        slideLeft = { left : '0px' };
 
-			switch( direction ) {
-				case 0:
-					// from top
-					fromStyle = !this.options.inverse ? slideFromTop : slideFromBottom;
-					toStyle = slideTop;
-					break;
-				case 1:
-					// from right
-					fromStyle = !this.options.inverse ? slideFromRight : slideFromLeft;
-					toStyle = slideLeft;
-					break;
-				case 2:
-					// from bottom
-					fromStyle = !this.options.inverse ? slideFromBottom : slideFromTop;
-					toStyle = slideTop;
-					break;
-				case 3:
-					// from left
-					fromStyle = !this.options.inverse ? slideFromLeft : slideFromRight;
-					toStyle = slideLeft;
-					break;
-			};
+      switch( direction ) {
+        case 0:
+          // from top
+          fromStyle = !this.options.inverse ? slideFromTop : slideFromBottom;
+          toStyle = slideTop;
+          break;
+        case 1:
+          // from right
+          fromStyle = !this.options.inverse ? slideFromRight : slideFromLeft;
+          toStyle = slideLeft;
+          break;
+        case 2:
+          // from bottom
+          fromStyle = !this.options.inverse ? slideFromBottom : slideFromTop;
+          toStyle = slideTop;
+          break;
+        case 3:
+          // from left
+          fromStyle = !this.options.inverse ? slideFromLeft : slideFromRight;
+          toStyle = slideLeft;
+          break;
+      };
 
-			return { from : fromStyle, to : toStyle };
+      return { from : fromStyle, to : toStyle };
 
-		},
-		// apply a transition or fallback to jquery animate based on Modernizr.csstransitions support
-		_applyAnimation : function( el, styleCSS, speed ) {
+    },
+    // apply a transition or fallback to jquery animate based on Modernizr.csstransitions support
+    _applyAnimation : function( el, styleCSS, speed ) {
 
-			$.fn.applyStyle = this.support ? $.fn.css : $.fn.animate;
-			el.stop().applyStyle( styleCSS, $.extend( true, [], { duration : speed + 'ms' } ) );
+      $.fn.applyStyle = this.support ? $.fn.css : $.fn.animate;
+      el.stop().applyStyle( styleCSS, $.extend( true, [], { duration : speed + 'ms' } ) );
 
-		},
+    },
 
-	};
+  };
 
-	var logError = function( message ) {
+  var logError = function( message ) {
 
-		if ( window.console ) {
+    if ( window.console ) {
 
-			window.console.error( message );
+      window.console.error( message );
 
-		}
+    }
 
-	};
+  };
 
-	$.fn.hoverdir = function( options ) {
+  $.fn.hoverdir = function( options ) {
 
-		var instance = $.data( this, 'hoverdir' );
+    var instance = $.data( this, 'hoverdir' );
 
-		if ( typeof options === 'string' ) {
+    if ( typeof options === 'string' ) {
 
-			var args = Array.prototype.slice.call( arguments, 1 );
+      var args = Array.prototype.slice.call( arguments, 1 );
 
-			this.each(function() {
+      this.each(function() {
 
-				if ( !instance ) {
+        if ( !instance ) {
 
-					logError( "cannot call methods on hoverdir prior to initialization; " +
-					"attempted to call method '" + options + "'" );
-					return;
+          logError( "cannot call methods on hoverdir prior to initialization; " +
+          "attempted to call method '" + options + "'" );
+          return;
 
-				}
+        }
 
-				if ( !$.isFunction( instance[options] ) || options.charAt(0) === "_" ) {
+        if ( !$.isFunction( instance[options] ) || options.charAt(0) === "_" ) {
 
-					logError( "no such method '" + options + "' for hoverdir instance" );
-					return;
+          logError( "no such method '" + options + "' for hoverdir instance" );
+          return;
 
-				}
+        }
 
-				instance[ options ].apply( instance, args );
+        instance[ options ].apply( instance, args );
 
-			});
+      });
 
-		}
-		else {
+    }
+    else {
 
-			this.each(function() {
+      this.each(function() {
 
-				if ( instance ) {
+        if ( instance ) {
 
-					instance._init();
+          instance._init();
 
-				}
-				else {
+        }
+        else {
 
-					instance = $.data( this, 'hoverdir', new $.HoverDir( options, this ) );
+          instance = $.data( this, 'hoverdir', new $.HoverDir( options, this ) );
 
-				}
+        }
 
-			});
+      });
 
-		}
+    }
 
-		return instance;
+    return instance;
 
-	};
+  };
 
 } )( jQuery, window );
 
@@ -230,7 +230,15 @@
 
 var main = function() {
 
+  var mQuery = Modernizr.mq('(min-width: 750px)');
+
+  lazyLoad();
   addClickEvents();
+
+  if (mQuery) {
+    positionCards();
+    animateCards();
+  }
 
   var resizeTimer;
 
@@ -245,13 +253,28 @@ var main = function() {
     }, 200);
   });
 
+  function lazyLoad() {
+
+    // https://www.sitepoint.com/five-techniques-lazy-load-images-website-performance/
+    [].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.onload = function() {
+        img.removeAttribute('data-src');
+      };
+    });
+  }
+
+  function testQuery() {
+    return mQuery = Modernizr.mq('(min-width: 750px)');
+  }
+
   function addClickEvents() {
 
-    var query = Modernizr.mq('(min-width: 750px)');
+    testQuery();
 
     $('.menu-button').on('click', function(e) {
       $('.menu').animate({left: 0}, 200);
-      if (query) {
+      if (mQuery) {
         $('body').animate({marginLeft: '320px'}, 250).addClass('open');
       } else {
         $('body').addClass('open');
@@ -259,7 +282,7 @@ var main = function() {
     });
     $('.close-button').on('click', function(e) {
       $('.menu').animate({left: '-320px'}, 200);
-      if (query) {
+      if (mQuery) {
         $('body').animate({marginLeft: 0}, 200).removeClass('open');
       } else {
         $('body').removeClass('open');
@@ -275,6 +298,20 @@ var main = function() {
   function removeClickEvents() {
     $('.menu-button').off('click');
     $('.close-button').off('click');
+  }
+
+  function positionCards() {
+    $('.gallery li').each(function(index, value) {
+      var topValue = ((index + 3) * 300) + "px";
+      $(this).css({top: topValue, opacity: 0});
+    });
+  }
+
+  function animateCards() {
+    $('.gallery li').each(function(index, value) {
+      var delayValue = (index * 40);
+      $(this).delay(delayValue).animate({top: 0, opacity: 1}, 400);
+    });
   }
 
 };
