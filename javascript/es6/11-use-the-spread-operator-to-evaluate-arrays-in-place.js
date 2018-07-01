45045362ed5eb0f28a942edbcf7948eb5fe82cbe
Stephen Bau <bauhouse@gmail.com>
2018-07-01 08:59:32 -0700

    Use spread operator in array literal

--- javascript/es6/11-use-the-spread-operator-to-evaluate-arrays-in-place.js
+++ javascript/es6/11-use-the-spread-operator-to-evaluate-arrays-in-place.js
@@ -1,7 +1,8 @@
 const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
-let arr2;
+let arr2 = ['JUN', 'JULY'];
+let arr3 = [...arr1, ...arr2];
 (function() {
   "use strict";
   arr2 = []; // change this line
 })();
-console.log(arr2);
+console.log(arr3);
