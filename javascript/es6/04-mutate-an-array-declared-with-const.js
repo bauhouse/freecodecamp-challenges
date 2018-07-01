2c63bbf98d4cbe184fc9cfc6d7f0eeef696671cb
Stephen Bau <bauhouse@gmail.com>
2018-07-01 07:23:38 -0700

    Complete challenge: Mutate an Array Declared with const

--- javascript/es6/04-mutate-an-array-declared-with-const.js
+++ javascript/es6/04-mutate-an-array-declared-with-const.js
@@ -4,7 +4,11 @@ function editInPlace() {
   // change code below this line
 
   // s = [2, 5, 7]; <- this is invalid
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
 
   // change code above this line
 }
 editInPlace();
+console.log(s);
