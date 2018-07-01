6bcae329bf72f1e42d8e59530fc6af4b0f2eb35b
Stephen Bau <bauhouse@gmail.com>
2018-07-01 09:18:03 -0700

    Copy all contents of arr1 into another array arr2 using the spread operator.

--- javascript/es6/11-use-the-spread-operator-to-evaluate-arrays-in-place.js
+++ javascript/es6/11-use-the-spread-operator-to-evaluate-arrays-in-place.js
@@ -1,8 +1,7 @@
 const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
-let arr2 = ['JUN', 'JULY'];
-let arr3 = [...arr1, ...arr2];
+let arr2;
 (function() {
   "use strict";
-  arr2 = []; // change this line
+  arr2 = [...arr1]; // change this line
 })();
-console.log(arr3);
+console.log(arr2);
