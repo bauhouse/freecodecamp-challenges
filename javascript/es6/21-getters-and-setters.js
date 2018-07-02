db398ba85b7245667962154ddd11b376e9fb9a77
Stephen Bau <bauhouse@gmail.com>
2018-07-02 12:42:02 -0700

    Attempt to use static methods in Thermostat class

--- javascript/es6/21-getters-and-setters.js
+++ javascript/es6/21-getters-and-setters.js
@@ -1,15 +1,42 @@
 function makeClass() {
   "use strict";
   /* Alter code below this line */
+  class Thermostat {
+    constructor(fahrenheit) {
+      this._fahrenheit = fahrenheit;
+      this._celsius = calculateCelsius(fahrenheit);
+    }
 
+    static calculateCelsius(F) {
+      return 5/9 * (F - 32);
+    }
+
+    static calculateFahrenheit(C) {
+      return C * 9.0 / 5 + 32;
+    }
+
+    // getter
+    get temperature(){
+      return this._celsius;
+    }
+    // setter
+    set temperature(celsius){
+      this._fahrenheit = calculateFahrenheit(celsius);
+      this._celsius = celsius;
+    }
+
+  }
   /* Alter code above this line */
   return Thermostat;
 }
 const Thermostat = makeClass();
 const thermos = new Thermostat(76); // setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in C
+console.log(temp);
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in C
+console.log(temp);
+
 
 
 /*
