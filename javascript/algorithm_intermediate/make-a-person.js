d81d1a8a3d835e62416f80476042963757f96ef7
Stephen Bau <bauhouse@gmail.com>
2018-07-02 07:16:33 -0700

    Complete challenge: Make a Person

--- javascript/algorithm_intermediate/make-a-person.js
+++ javascript/algorithm_intermediate/make-a-person.js
@@ -1,10 +1,51 @@
 var Person = function(firstAndLast) {
   // Complete the method below and implement the others similarly
-  this.getFullName = function() {
-    return "";
+
+  var firstName = firstAndLast.split(' ')[0];
+  var lastName = firstAndLast.split(' ')[1];
+  var fullName = [firstName, lastName].join(' ');
+
+  this.setFirstName = function(str) {
+    firstName = str;
+    updateFullName();
   };
-  return firstAndLast;
+
+  this.setLastName = function(str) {
+    lastName = str;
+    updateFullName();
+  };
+
+  this.setFullName = function(str) {
+    firstName = str.split(' ')[0];
+    lastName = str.split(' ')[1];
+    updateFullName();
+  };
+
+  this.getFirstName = function() {
+    return firstName;
+  };
+
+  this.getLastName = function() {
+    return lastName;
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  function updateFullName() {
+    return fullName = [firstName, lastName].join(' ');
+  }
+
 };
 
 var bob = new Person('Bob Ross');
 bob.getFullName();
+console.log(bob.getFullName());
+console.log(Object.keys(bob).length);
+console.log(Object.keys(bob));
+console.log(bob instanceof Person);
+console.log(bob.getFirstName());
+bob.setFirstName('Haskell');
+console.log(bob.getFullName());
+console.log(bob.getFirstName());
