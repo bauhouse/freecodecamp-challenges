7a07ae270ce262b3c01b7710a226152d6886eff5
Stephen Bau <bauhouse@gmail.com>
2018-07-03 14:13:08 -0700

    Fix the errors so that it is valid JSX and successfully transpiles

--- frontend/react/src/index.js
+++ frontend/react/src/index.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 const JSX = (
-  <div className="myDiv">
-    <h1>Add a class to this div</h1>
+  <div>
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
   </div>
 );
 
