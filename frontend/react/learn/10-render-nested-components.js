c1216e29cec46c52059a023e5c7a32e325d0442e
Stephen Bau <bauhouse@gmail.com>
2018-07-03 15:02:16 -0700

    Nest components inside the parent components

--- frontend/react/learn/10-render-nested-components.js
+++ frontend/react/learn/10-render-nested-components.js
@@ -15,9 +15,7 @@ const TypesOfFruit = () => {
 const Fruits = () => {
   return (
     <div>
-      { /* change code below this line */ }
-
-      { /* change code above this line */ }
+      <TypesOfFruit />
     </div>
   );
 };
@@ -31,9 +29,7 @@ class TypesOfFood extends React.Component {
     return (
       <div>
         <h1>Types of Food:</h1>
-        { /* change code below this line */ }
-
-        { /* change code above this line */ }
+        <Fruits />
       </div>
     );
   }
--- frontend/react/src/index.js
+++ frontend/react/src/index.js
@@ -1,27 +1,41 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-
-const ChildComponent = () => {
+const TypesOfFruit = () => {
   return (
     <div>
-      <p>I am the child</p>
+      <h2>Fruits:</h2>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
     </div>
   );
 };
 
-class ParentComponent extends React.Component {
+const Fruits = () => {
+  return (
+    <div>
+      <TypesOfFruit />
+    </div>
+  );
+};
+
+class TypesOfFood extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div>
-        <h1>I am the parent</h1>
-        <ChildComponent />
+        <h1>Types of Food:</h1>
+        <Fruits />
       </div>
     );
   }
 };
 
-ReactDOM.render(<ParentComponent />, document.getElementById('root'));
+ReactDOM.render(<TypesOfFood />, document.getElementById('root'));
