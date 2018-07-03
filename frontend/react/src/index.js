a49d067581d0d013572fc65634ef4977cfaf7a52
Stephen Bau <bauhouse@gmail.com>
2018-07-03 10:54:04 -0700

    Modify src files to render the first freeCodeCamp challenge
    React: Create a Simple JSX Element

--- frontend/react/src/index.js
+++ frontend/react/src/index.js
@@ -1,1 +1,6 @@
-import FormContainer from "./js/components/container/FormContainer";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const JSX = <h1>Hello JSX!</h1>;
+
+ReactDOM.render(JSX, document.getElementById('root'));
