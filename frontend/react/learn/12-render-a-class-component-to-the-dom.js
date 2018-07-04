cb6b08aa453ccddc6d6107c1b461221b9b92d74d
Stephen Bau <bauhouse@gmail.com>
2018-07-04 08:15:06 -0700

    React: Render a Class Component to the DOM

---
+++ frontend/react/learn/12-render-a-class-component-to-the-dom.js
@@ -0,0 +1,18 @@
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* change code below this line */}
+
+        {/* change code above this line */}
+      </div>
+    );
+  }
+};
+
+// change code below this line
