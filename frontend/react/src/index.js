4460772a3800885292b0fcdabe526000e6abaef2
Stephen Bau <bauhouse@gmail.com>
2018-07-09 17:31:45 -0700

    Render an instance of the ReturnTempPassword component in the parent component ResetPassword

--- frontend/react/src/index.js
+++ frontend/react/src/index.js
@@ -1,27 +1,39 @@
 import React, { Component, PropTypes } from "react";
 import ReactDOM from "react-dom";
 
-const Items = (props) => {
-  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
-};
-
-// change code below this line
-Items.propTypes = {
-  quantity: PropTypes.number.isRequired
-}
-// change code above this line
-
-Items.defaultProps = {
-  quantity: 0
-}
-
-class ShoppingCart extends React.Component {
+class ReturnTempPassword extends React.Component {
   constructor(props) {
     super(props);
+
   }
   render() {
-    return <Items />
+    return (
+        <div>
+            { /* change code below this line */ }
+            <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
+            { /* change code above this line */ }
+        </div>
+    );
   }
 };
 
-ReactDOM.render(<ShoppingCart />, document.getElementById('challenge-node'));
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          <h2>Reset Password</h2>
+          <h3>We've generated a new temporary password for you.</h3>
+          <h3>Please reset this password from your account settings ASAP.</h3>
+          { /* change code below this line */ }
+          <ReturnTempPassword tempPassword={"g4h8yj92"} />
+          { /* change code above this line */ }
+        </div>
+    );
+  }
+};
+
+ReactDOM.render(<ResetPassword />, document.getElementById('challenge-node'));
