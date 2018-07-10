a6641658cff04f9dd88e82e9e9ce22ae01707f43
Stephen Bau <bauhouse@gmail.com>
2018-07-10 09:29:39 -0700

    React: Use Advanced JavaScript in React Render Method

--- frontend/react/learn/39-advanced-javascript-in-render-method.js
+++ frontend/react/learn/39-advanced-javascript-in-render-method.js
@@ -1,7 +1,3 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
-
 const inputStyle = {
   width: 235,
   margin: 5
@@ -74,5 +70,3 @@ class MagicEightBall extends React.Component {
     );
   }
 };
-
-ReactDOM.render(<MagicEightBall />, document.getElementById('root'));
