d68d8167915c60769aa7224d1b03c8ada28df7e6
Stephen Bau <bauhouse@gmail.com>
2018-07-09 17:37:28 -0700

    React: Review Using Props with Stateless Functional Components

---
+++ frontend/react/learn/20-props-and-components.js
@@ -0,0 +1,13 @@
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// change code below this line
