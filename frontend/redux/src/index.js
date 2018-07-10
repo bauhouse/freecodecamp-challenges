8168f6d12e14c79c1ed1e690e0460026df83d1b0
Stephen Bau <bauhouse@gmail.com>
2018-07-10 14:22:37 -0700

    Set up React development environment for Redux challenges

---
+++ frontend/redux/src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div/>
+  }
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
