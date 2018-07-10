a2c83b596377ec9ec8c24654c13b61027150e776
Stephen Bau <bauhouse@gmail.com>
2018-07-10 11:56:41 -0700

    React: Render React on the Server with renderToString

--- frontend/react/src/index.js
+++ frontend/react/src/index.js
@@ -2,50 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
-class MyComponent extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      users: [
-        {
-          username: 'Jeff',
-          online: true
-        },
-        {
-          username: 'Alan',
-          online: false
-        },
-        {
-          username: 'Mary',
-          online: true
-        },
-        {
-          username: 'Jim',
-          online: false
-        },
-        {
-          username: 'Sara',
-          online: true
-        },
-        {
-          username: 'Laura',
-          online: true
-        }
-      ]
-    }
   }
   render() {
-    const usersOnline = this.state.users.filter(user => user.online);
-    const renderOnline = usersOnline.map((user, index) => <li key={'user' + index}>{user.username}</li>)
-    return (
-       <div>
-         <h1>Current Online Users:</h1>
-         <ul>
-           {renderOnline}
-         </ul>
-       </div>
-    );
+    return <div/>
   }
 };
+
+// change code below this line
 
 ReactDOM.render(<MyComponent />, document.getElementById('root'));
