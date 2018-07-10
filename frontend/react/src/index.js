0abf47cae5723f85cb45fc2ce2a688516de8df58
Stephen Bau <bauhouse@gmail.com>
2018-07-10 09:16:18 -0700

    Set the style attribute equal to the styles constant

--- frontend/react/src/index.js
+++ frontend/react/src/index.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 
-// const styles =
+const styles = {
+  color: "purple",
+  fontSize: 40,
+  border: "2px solid purple"
+};
 // change code above this line
 class Colorful extends React.Component {
   render() {
     // change code below this line
     return (
-      <div style={{color: "yellow", fontSize: 24}}>Style Me!</div>
+      <div style={styles}>Style Me!</div>
     );
     // change code above this line
   }
