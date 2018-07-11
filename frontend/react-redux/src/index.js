1b01816e2c50167eac5debe28427a3b2eda7e1c9
Stephen Bau <bauhouse@gmail.com>
2018-07-10 21:45:57 -0700

    React and Redux: Getting Started with React Redux

---
+++ frontend/react-redux/src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import * as Redux from 'redux';
+
+class DisplayMessages extends React.Component {
+  // change code below this line
+
+  // change code above this line
+  render() {
+    return <div />
+  }
+};
