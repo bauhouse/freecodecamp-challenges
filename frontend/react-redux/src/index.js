b4cff0b9590f5ee385a7cc7e6d80c36648ff6327
Stephen Bau <bauhouse@gmail.com>
2018-07-11 08:15:09 -0700

    Map over the array of messages and render it to the screen

--- frontend/react-redux/src/index.js
+++ frontend/react-redux/src/index.js
@@ -10,17 +10,37 @@ class DisplayMessages extends React.Component {
       input: '',
       messages: []
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
   }
-  // add handleChange() and submitMessage() methods here
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  submitMessage(event) {
+    this.setState({
+      input: '',
+      messages: [...this.state.messages].concat(this.state.input)
+    });
+  }
 
   render() {
+    const items = this.state.messages.map((value, index) => <li key={'item' + index}>{value}</li>);
     return (
       <div>
         <h2>Type in a new Message:</h2>
-        { /* render an input, button, and ul here */ }
-
-        { /* change code above this line */ }
+        <input value={this.state.input} onChange={this.handleChange} />
+        <button onClick={this.submitMessage}>Add Message</button>
+        <p>{this.state.input}</p>
+        <ul>
+          {items}
+        </ul>
       </div>
     );
   }
 };
+
+ReactDOM.render(<DisplayMessages />, document.getElementById('root'));
