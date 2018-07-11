e5445438ddfa271832dd03b01f9596231088b767
Stephen Bau <bauhouse@gmail.com>
2018-07-11 07:41:17 -0700

    Add a constructor to the component and initialize it

--- frontend/react-redux/src/index.js
+++ frontend/react-redux/src/index.js
@@ -5,7 +5,13 @@ import * as Redux from 'redux';
 
 class DisplayMessages extends React.Component {
   // change code below this line
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+  }
   // change code above this line
   render() {
     return <div />
