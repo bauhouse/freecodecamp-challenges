d6aeb4f9b5b850fc6e5f07f5049a44dc14ddccaf
Stephen Bau <bauhouse@gmail.com>
2018-07-10 20:52:42 -0700

    Return a new array with the item from action.todo appended to the end

--- frontend/redux/learn/14-never-mutate-state.js
+++ frontend/redux/learn/14-never-mutate-state.js
@@ -12,7 +12,7 @@ const immutableReducer = (state = todos, action) => {
   switch(action.type) {
     case ADD_TO_DO:
       // don't mutate state here or the tests will fail
-      return
+      return state.concat(action.todo);
     default:
       return state;
   }
@@ -22,7 +22,7 @@ const immutableReducer = (state = todos, action) => {
 const addToDo = (todo) => {
   return {
     type: ADD_TO_DO,
-    todo
+    todo: todo
   }
 }
 
