c9b70c9291d7f8eb366f7a6b1dc4d17d685597d0
Glitch (fcc-api-microservices-app) <none>
2019-04-14 20:42:56 +0000

    Add an environment variable as a configuration option

--- myApp.js
+++ myApp.js
@@ -24,17 +24,26 @@ app.get("/", function(req, res) {
   res.sendFile(absolutePath);
 });
 
-
 /** 4) Serve static assets  */
 app.use( express.static( __dirname + "/public" ) );
 
 /** 5) serve JSON on a specific route */
+/*
 app.get("/json", function(req, res) {
   res.json( {"message": "Hello json"} );
 });
+*/
 
 /** 6) Use the .env file to configure the app */
- 
+app.get("/json", function(req, res) {
+
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json( {"message": message} );
+});
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
