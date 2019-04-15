93d86e2356a6f17b0d13c89bf3fbcba3672729a0
Glitch (fcc-api-microservices-db) <none>
2019-04-15 19:51:51 +0000

    Perform classic updates by running find, edit, then save

--- myApp.js
+++ myApp.js
@@ -223,8 +223,21 @@ var findPersonById = function(personId, done) {
 var findEditThenSave = function(personId, done) {
   var foodToAdd = 'hamburger';
   
-  done(null/*, data*/);
+  Person.findById(personId, function(err, data) {
+    if(err) {
+      done(err);
+    }
+    data.favoriteFoods.push(foodToAdd);
+    data.save(function(err, data) {
+      if(err) {
+        done(err);
+      }
+      done(null, data);
+    });
+  });  
+
 };
+
 
 /** 9) New Update : Use `findOneAndUpdate()` */
 
