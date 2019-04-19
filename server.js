757e05c19b251e30f3ea5039c31c0a42a94dae82
Glitch (fcc-file-metadata-app) <none>
2019-04-19 22:18:29 +0000

    File Metadata Microservice

--- server.js
+++ server.js
@@ -1,64 +1,23 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+'use strict';
 
-const cors = require('cors')
+var express = require('express');
+var cors = require('cors');
 
-const mongoose = require('mongoose')
-// mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track' )
+// require and use "multer"...
 
-// Connect to Database
-var database = mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, function(error) {
-  if(error) {
-    console.log(error);
-  } else {
-    console.log("connection successful");
-  }
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
 });
 
-app.use(cors())
-
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
-
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
-
-// Database schemas
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-  username: String 
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
 });
