76eb59ef708b582f689c372e15beebbc9d608f60
Glitch (fcc-file-metadata-app) <none>
2019-04-19 22:36:40 +0000

    Return JSON on POST request to /api/fileanalyse

--- server.js
+++ server.js
@@ -21,3 +21,8 @@ app.get('/hello', function(req, res){
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
+
+app.post('/api/fileanalyse', function(req, res) {
+  console.log("Upload file");
+  res.json({filename: "filename", filesize: 12345, filetype: 'PDF'});
+});
