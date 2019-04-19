c1b8eb9d89f17f38771be9b93664d206c500afdf
Glitch (fcc-exercise-rest-api) <none>
2019-04-19 18:31:22 +0000

    Reorder not found and error handling

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const cors = require('cors')
+const cors = require('cors');
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 // mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track' )
 
 // Connect to Database
@@ -16,45 +16,20 @@ var database = mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, 
   }
 });
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
-
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
 
 // Database schemas
 var Schema = mongoose.Schema;
@@ -98,6 +73,7 @@ var saveExercise = function(exercise, done) {
 // Create new user
 var handleNewUser = function(req, res) {
   console.log("Create new user");
+  return res.json({event: "Create new user"});
 }
 
 app.post("/api/exercise/new-user", handleNewUser);
@@ -105,6 +81,7 @@ app.post("/api/exercise/new-user", handleNewUser);
 // Log exercise activity
 var handleAddExercise = function(req, res) {
   console.log("Log exercise activity");
+  return res.json({event: "Log exercise activity"});
 }
 
 app.post("/api/exercise/add", handleAddExercise);
@@ -112,6 +89,32 @@ app.post("/api/exercise/add", handleAddExercise);
 // Display exercise log
 var handleExerciseLog = function(req, res) {
   console.log("Display exercise log");
+  return res.json({event: "Display exercise log"});
 }
 
 app.get("/api/exercise/log", handleExerciseLog);
+
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'});
+});
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+});
+
